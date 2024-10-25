9796a6b807c2884854146f316358189d5de54e5d
v.lavrinenko <v.lavrinenko@framework.team>
2024-10-25 10:59:04 +0400

    5

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# генерация списка из 10 случайных цифр и вывод их
-
 import random
 
 random_numbers = []
@@ -7,4 +5,11 @@ random_numbers = []
 for i in range(10):
 	random_numbers.append(random.randint(0, 10))
 
+# сортировка списка и вывод min и max
+
+random_numbers.sort()
+min_number = min(random_numbers)
+max_number = max(random_numbers)
+
 print(random_numbers)
+print(f'Мин: {min_number}, Макс: {max_number}')
