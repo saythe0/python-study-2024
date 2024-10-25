2f3918d4d92f6f7c8d32ef84f3df0a3c7a82a13d
v.lavrinenko <v.lavrinenko@framework.team>
2024-10-25 10:59:17 +0400

    6

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ random_numbers = []
 for i in range(10):
 	random_numbers.append(random.randint(0, 10))
 
-# сортировка списка и вывод min и max
-
 random_numbers.sort()
 min_number = min(random_numbers)
 max_number = max(random_numbers)
 
 print(random_numbers)
 print(f'Мин: {min_number}, Макс: {max_number}')
+
+# посчитать числа в списке и вывод суммы через For
+
+sum_numbers = 0
+
+for number in random_numbers:
+    sum_numbers += number
+
+print(f'Сумма: {sum_numbers}')
