22251b923f2f9d370ea4bb3491eecc6adad61182
v.lavrinenko <v.lavrinenko@framework.team>
2024-10-30 15:14:39 +0400

    10

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import random
-import statistics
-import tkinter
-import tkinter.filedialog
+# создать функцию и вызвать её
 
-# добавить выбор файла через проводник
-file_path = tkinter.filedialog.askopenfilename(title="Выберите файл", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+def dog():
+	print('gav gav gav')
 
-
-file = open(file_path, "w+")
-
-for i in range(10):
-	file.write(f'{random.randint(0, 10)} ')
-
-file.seek(0)
-
-numbers = []
-for num in file.read().strip().split():
-	numbers.append(int(num))
-	
-avg = statistics.mean(numbers)
-print(numbers)
-print(avg)
-file.close()
+dog()
